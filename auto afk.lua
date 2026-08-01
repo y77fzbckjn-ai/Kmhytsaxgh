2e5e47ffde409fd6e4560fde325bb4a09907433b
y77fzbckjn-ai <y77fzbckjn@privaterelay.appleid.com>
2026-08-01 19:45:34 +0800

    Update auto afk.lua

--- auto afk.lua
+++ auto afk.lua
@@ -306,7 +306,7 @@ local function applyFpsBoost()
             textLabel.Position = UDim2.new(0.5, 0, 0.95, 0)
             textLabel.AnchorPoint = Vector2.new(0.5, 1)
             textLabel.BackgroundTransparency = 1
-            textLabel.Text = "AFK MODE ACTIVE\nFENG JIU MY BINI JIR"
+            textLabel.Text = "AFK MODE ACTIVE\n VITA CANGTIP SEKALI"
             textLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
             textLabel.TextScaled = true
             textLabel.TextWrapped = true
